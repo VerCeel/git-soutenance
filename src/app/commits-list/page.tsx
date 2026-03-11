d2002ce0289a8da13a2c67d254adba434f40601d
VerCeel <hirich62@gmail.com>
2026-03-11 18:12:10 +0100

    feat: add new pages for commits list, contact, and login

---
+++ src/app/commits-list/page.tsx
@@ -0,0 +1,7 @@
+const pages = () => {
+  return (
+    <div className="h-screen flex items-center justify-center">pages</div>
+  )
+}
+
+export default pages
---
+++ src/app/contact/page.tsx
@@ -0,0 +1,7 @@
+const page = () => {
+  return (
+    <div className="h-screen flex items-center justify-center">pages</div>
+  )
+}
+
+export default page
