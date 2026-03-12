3c0891ede899d93b9ef89230686430e6e4481270
VerCeel <hirich62@gmail.com>
2026-03-12 08:46:35 +0100

    feat: Add links and map(links)

--- src/components/static/Navbar.tsx
+++ src/components/static/Navbar.tsx
@@ -2,19 +2,45 @@ import { Github, User } from "lucide-react";
 import Link from "next/link";
 import { Button } from "../ui/button";
 
+const links = [
+  {
+    href: "/",
+    label: "Home",
+  },
+  {
+    href: "/commits-list",
+    label: "Commits List",
+  },
+  {
+    href: "/contact",
+    label: "Contact",
+  },
+  {
+    href: "/login",
+    label: "Login",
+  },
+];
+
 const NavBar = () => {
   return (
-    <div className="fixed text-neutral-200 top-0 right-2 left-2 w-auto shadow z-50 flex items-center justify-between px-4 py-3 my-2 rounded-lg border border-white/25 bg-white/50 backdrop-blur-3xl">
-      <div className="flex gap-2 items-center">
+    <div className="fixed text-neutral-200 top-4 shadow-lg ring-1 ring-black/5 right-4 left-4 w-auto z-50 flex items-center justify-between px-6 py-3 rounded-lg border border-white/10 bg-black/20 backdrop-blur-xl">
+      <div className="flex text-xl gap-2 items-center">
         <Github />
         Github
       </div>
 
-      <div className="flex  items-center gap-4">
-        <Button className="hover:bg-white/40 hover:text-white" variant="ghost"><Link href="/">Home</Link></Button>
-        <Button className="hover:bg-white/40 hover:text-white" variant="ghost"><Link href="/commits-list">commits list</Link></Button>
-        <Button className="hover:bg-white/40 hover:text-white" variant="ghost"><Link href="/contact">Contact</Link></Button>
-        <Button className="px-2"><Link className="flex items-center gap-1" href="/login"><User />Login</Link></Button>
+      <div className="flex items-center gap-4">
+        {links.map((link, index) => (
+          <div className="text-neutral-300 hover:text-white">
+            <Link key={index} href={link.href}>{link.label}</Link>
+          </div>
+        ))}
+        <Button size="lg" className="px-2">
+          <Link className="flex items-center gap-2" href="/login">
+            <User />
+            Login
+          </Link>
+        </Button>
       </div>
     </div>
   );
